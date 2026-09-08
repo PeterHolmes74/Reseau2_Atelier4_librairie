2a94750ea84bffdf4e042ec5acb934765cb98643
yannick <yannickchauvette@icloud.com>
2026-09-08 17:10:55 -0400

    Mise a jours des tests

--- arraygenerate_test.go
+++ arraygenerate_test.go
@@ -8,7 +8,7 @@ func TestArrayGenerate(t *testing.T) {
 	attendu := len(ArrayGenerate(-1)) == 0
 
 	if !attendu {
-		t.Errorf("ArrayGenerate de mauvaise taille")
+		t.Errorf("ArrayGenerate de mauvaise taille %d", len(ArrayGenerate(-1)))
 	}
 }
 
@@ -16,6 +16,6 @@ func TestArrayGenerate2(t *testing.T) {
 	obtenu := ArrayGenerate(1)
 
 	if !(obtenu[0] >= 1 && obtenu[0] <= 10000) {
-		t.Errorf("Obtenu de mauvaise taille")
+		t.Errorf("Contenu de ArrayGenerate de mauvaise taille %d", obtenu[0])
 	}
 }
